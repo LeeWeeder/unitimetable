ceed23d60a5661648faaa4ea7d31de681c13e31f
LeeWeeder <ljmaq239@gmail.com>
2025-02-22 00:09:16 +0800

    Bump app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.leeweeder.unitimetable"
         minSdk = 30
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.0.1alpha01"
+        versionCode = 2
+        versionName = "0.0.1alpha02"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
