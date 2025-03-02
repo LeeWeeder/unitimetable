e99cc9e5e8275834c48f4354ad73ed2fd0acc28b
LeeWeeder <ljmaq239@gmail.com>
2025-03-02 10:28:25 +0800

    Add selected timetable preferences

--- app/src/main/java/com/leeweeder/unitimetable/domain/repository/DataStoreRepository.kt
+++ app/src/main/java/com/leeweeder/unitimetable/domain/repository/DataStoreRepository.kt
@@ -6,5 +6,9 @@ import kotlinx.coroutines.flow.Flow
 interface DataStoreRepository {
     val timeTablePrefFlow: Flow<TimeTablePref>
 
+    val selectedTimetableIdFlow: Flow<Int>
+
     suspend fun setMainTimeTableId(id: Int)
+
+    suspend fun setSelectedTimetable(id: Int)
 }
